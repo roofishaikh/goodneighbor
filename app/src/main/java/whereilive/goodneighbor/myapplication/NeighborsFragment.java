2c9e9ed407ccf4bae007e4547640427500a40d8c
Roofi Shaikh <roofishaikh@gmail.com>
2015-04-12 10:18:25 -0500

    Added NeighborsFragment

---
+++ app/src/main/java/whereilive/goodneighbor/myapplication/NeighborsFragment.java
@@ -0,0 +1,9 @@
+package whereilive.goodneighbor.myapplication;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by roofishaikh on 4/12/15.
+ */
+public class NeighborsFragment extends Fragment {
+}
