b024946f0b00e4a4f78c9e6aaa8dc178070d6299
zeitlerc <zeitlerc@gmail.com>
2015-04-12 10:26:55 -0500

    Using filled out NeighborFragment

--- app/src/main/java/whereilive/goodneighbor/myapplication/MainActivity.java
+++ app/src/main/java/whereilive/goodneighbor/myapplication/MainActivity.java
@@ -51,8 +51,8 @@ public class MainActivity extends ActionBarActivity implements InfoFragment.OnFr
                         this, "Info", InfoFragment.class));
         actionBar.addTab(tab2);
         ActionBar.Tab tab3= actionBar.newTab()
-                .setIcon(R.drawable.person).setTabListener(new TabListener<NeighborsFragment>(
-                        this, "Neighbors", NeighborsFragment.class));
+                .setIcon(R.drawable.person).setTabListener(new TabListener<NeighborFragment>(
+                        this, "Neighbors", NeighborFragment.class));
 
         actionBar.addTab(tab3);
         ActionBar.Tab tab4= actionBar.newTab()
--- app/src/main/java/whereilive/goodneighbor/myapplication/NeighborsFragment.java
+++
@@ -1,9 +0,0 @@
-package whereilive.goodneighbor.myapplication;
-
-import android.support.v4.app.Fragment;
-
-/**
- * Created by roofishaikh on 4/12/15.
- */
-public class NeighborsFragment extends Fragment {
-}
